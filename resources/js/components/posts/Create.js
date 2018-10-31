814c9929c8c000c822d8055eb1e4079f9a07829f
Tom Geitz <>
2018-10-31 14:04:26 -0500

    Create Create Post component

---
+++ resources/js/components/posts/Create.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Create extends Component {
+    state = {  }
+    render() {
+        return (
+            <div>Create post</div>
+        );
+    }
+}
+
+export default Create;

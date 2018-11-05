712b34932ae3b1e3c309cf5dc5732d891118aa3a
Tom Geitz <>
2018-11-05 13:16:09 -0600

    Adjust Register component to use MUI

--- resources/js/components/auth/Register.js
+++ resources/js/components/auth/Register.js
@@ -2,23 +2,76 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import compose from 'recompose/compose';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Avatar from '@material-ui/core/Avatar';
+import LockIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import withStyles from '@material-ui/core/styles/withStyles';
 
 import { registerUser } from "../../actions";
+
+const styles = theme => ({
+    layout: {
+        width: 'auto',
+        display: 'block', // Fix IE 11 issue.
+        marginLeft: theme.spacing.unit * 3,
+        marginRight: theme.spacing.unit * 3,
+        [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+            width: 400,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+        },
+    },
+    paper: {
+        marginTop: theme.spacing.unit * 8,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
+    },
+    avatar: {
+        margin: theme.spacing.unit,
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing.unit,
+    },
+    submit: {
+        marginTop: theme.spacing.unit * 3,
+    },
+});
 
 class Register extends Component {
     renderField(field) {
         const { meta: { touched, error } } = field;
         const className = `form-group ${touched && error ? 'has-danger' : ''}`;
         return (
-            <div className={className}>
-                <label>{ field.label }</label>
-                <input
-                    className="form-control"
+            <FormControl margin="normal" required fullWidth>
+                <InputLabel htmlFor={field.type}>{field.label}</InputLabel>
+                <Input 
+                    name={field.name}
                     type={field.type}
+                    autoFocus={field.autoFocus}
                     {...field.input}
                 />
                 {touched ? error : ''}
-            </div>
+            </FormControl>
+            // <div className={className}>
+            //     <label>{field.label}</label>
+            //     <input
+            //         className="form-control"
+            //         type={field.type}
+            //         {...field.input}
+            //     />
+            //     {touched ? error : ''}
+            // </div>
         )
     }
 
@@ -27,37 +80,63 @@ class Register extends Component {
     }
 
     render() {
-        const { handleSubmit } = this.props;
+        const { handleSubmit, classes } = this.props;
 
         return (
-            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
-                <Field
-                    label="Name"
-                    name="name"
-                    type="text"
-                    component={this.renderField} 
-                />
-                <Field
-                    label="Email"
-                    name="email"
-                    type="text"
-                    component={this.renderField} 
-                />
-                <Field
-                    label="Password"
-                    name="password"
-                    type="password"
-                    component={this.renderField} 
-                />
-                <Field
-                    label="Confirm Password"
-                    name="password_confirmation"
-                    type="password"
-                    component={this.renderField} 
-                />
-                <button type="submit" className="btn btn-primary">Create</button>
-                <Link to="/posts" className="btn btn-secondary">Cancel</Link>
-            </form>
+            <React.Fragment>
+                <main className={classes.layout}>
+                    <Paper className={classes.paper}>
+                        <Avatar className={classes.avatar}>
+                            <LockIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                        <form className={classes.form} onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+                            <Field
+                                label="Name"
+                                name="name"
+                                type="text"
+                                component={this.renderField}
+                            />
+                            <Field
+                                label="Email"
+                                name="email"
+                                type="text"
+                                component={this.renderField}
+                            />
+                            <Field
+                                label="Password"
+                                name="password"
+                                type="password"
+                                component={this.renderField}
+                            />
+                            <Field
+                                label="Confirm Password"
+                                name="password_confirmation"
+                                type="password"
+                                component={this.renderField}
+                            />
+                            <Button
+                                type="submit"
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                            >
+                                Submit
+                            </Button>
+                            <Button
+                                component={Link}
+                                to="/posts"
+                                color="secondary"
+                                className={classes.submit}
+                            >
+                                Cancel
+                            </Button>
+                        </form>
+                    </Paper>
+                </main>
+            </React.Fragment>
         );
     }
 }
@@ -82,9 +161,11 @@ function validate(values) {
     return errors;
 }
 
-export default reduxForm({
-    validate,
-    form: 'RegisterUserForm'
-})(
-    connect(null, { registerUser })(Register)
-);
+export default compose(
+    withStyles(styles, { name: 'Register' }),
+    reduxForm({
+        validate,
+        form: 'RegisterUserForm'
+    }),
+    connect(null, { registerUser })
+)(Register);

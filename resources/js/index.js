2beadf54091928d31c66f384431c1536326606c4
Tom Geitz <Tom@Toms-MacBook-Pro.local>
2018-11-07 16:45:43 -0600

    Move redux create store to index instead of app component

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import thunk from 'redux-thunk';
 import App from './src/components/App';
+import reducers from './src/reducers/reducers';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const createStoreWithMiddleware = applyMiddleware(promise, thunk)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.querySelector('#app'));
--- resources/js/src/components/App.js
+++ resources/js/src/components/App.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import promise from 'redux-promise';
-import thunk from 'redux-thunk';
 
-import reducers from '../reducers/reducers';
 import Register from './auth/Register';
 import Login from './auth/Login';
 import PostsIndex from './posts/Index';
@@ -14,8 +9,6 @@ import { withStyles } from '@material-ui/core/styles';
 import withRoot from '../../withRoot';
 import AppBarTop from './AppBar';
 
-const createStoreWithMiddleware = applyMiddleware(promise, thunk)(createStore);
-
 const styles = theme => ({
 
 });
@@ -23,19 +16,17 @@ const styles = theme => ({
 class App extends Component {
     render() {
         return (
-            <Provider store={createStoreWithMiddleware(reducers)}>
-                <BrowserRouter>
-                    <React.Fragment>
-                        <AppBarTop />
-                        <Switch>
-                            <Route exact path="/register" component={Register} />
-                            <Route exact path="/login" component={Login} />
-                            <Route exact path="/posts/create" component={PostsCreate} />
-                            <Route exact path="/posts/" component={PostsIndex} />
-                        </Switch>
-                    </React.Fragment>
-                </BrowserRouter>
-            </Provider>
+            <BrowserRouter>
+                <React.Fragment>
+                    <AppBarTop />
+                    <Switch>
+                        <Route exact path="/register" component={Register} />
+                        <Route exact path="/login" component={Login} />
+                        <Route exact path="/posts/create" component={PostsCreate} />
+                        <Route exact path="/posts/" component={PostsIndex} />
+                    </Switch>
+                </React.Fragment>
+            </BrowserRouter>
         );
     }
 }

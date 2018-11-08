13098e517df837f8089267a21685b02128b66a5b
Tom Geitz <Tom@Toms-MacBook-Pro.local>
2018-11-08 12:24:17 -0600

    Add user menu with logout button to appbar when logged in

--- resources/js/src/components/AppBar.js
+++ resources/js/src/components/AppBar.js
@@ -8,10 +8,12 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import MenuIcon from '@material-ui/icons/Menu';
 
-import { getCurrentUserData } from '../actions'
-import LogoutButton from './auth/LogoutButton';
+import { getCurrentUserData, logoutUser } from '../actions'
 
 const styles = {
     root: {
@@ -24,12 +26,54 @@ const styles = {
         marginLeft: -12,
         marginRight: 20,
     },
+    toolbar: {
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    auth: {
+        overflow: 'auto',
+        whiteSpace: 'nowrap'
+    },
+    authText: {
+        display: 'inline-block',
+        paddingTop: 10
+    },
+    authMenuButton: {
+        display: 'inline-block',
+        paddingBottom: 18
+    }
 };
 
 
 class AppBarTop extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            anchorEl: null
+        }
+    }
+
+    handleChange(event) {
+        this.setState({ auth: event.target.checked });
+    };
+
+    handleMenu(event) {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
+    handleClose() {
+        this.setState({ anchorEl: null });
+    };
+
+    handleLogout() {
+        return this.props.logoutUser();
+        // this.handleClose();
+    }
+
     renderAuth() {
         const { classes } = this.props;
+        const { anchorEl } = this.state;
+        const open = Boolean(anchorEl);
 
         if (!this.props.auth.isAuthenticated) {
             return (
@@ -40,11 +84,43 @@ class AppBarTop extends Component {
             );
         } else {
             return (
-                <div>
-                    <LogoutButton color="inherit" />
-                    <Typography variant="h6" align="right" color="inherit">
-                        Welcome, {this.props.auth.currentUser.name}!
+                <div className={classes.auth}>
+                    {/* <div>
+                        <LogoutButton color="inherit" />
+                    </div>
+                    <div>
+                        <Typography variant="h6" align="right" color="inherit">
+                            Welcome, {this.props.auth.currentUser.name}!
+                        </Typography>
+                    </div> */}
+                    <Typography className={classes.authText} variant="h6" align="right" color="inherit">
+                        {this.props.auth.currentUser.name}
                     </Typography>
+                    <IconButton
+                        className={classes.authMenuButton}
+                        aria-owns={open ? 'menu-appbar' : undefined}
+                        aria-haspopup="true"
+                        onClick={this.handleMenu.bind(this)}
+                        color="inherit"
+                    >
+                        <AccountCircle />
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={open}
+                        onClose={this.handleClose.bind(this)}
+                    >
+                        <MenuItem onClick={this.handleLogout.bind(this)}>Log Out</MenuItem>
+                    </Menu>
                 </div>
             )
         }
@@ -56,15 +132,17 @@ class AppBarTop extends Component {
         return (
             <div className={classes.root}>
                 <AppBar position="static">
-                    <Toolbar>
-                        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-                            <MenuIcon />
-                        </IconButton>
-                        <Button color="inherit" align="left" component={Link} to="/posts">
-                            <Typography variant="h6" color="inherit" className={classes.grow}>
-                                Posts
+                    <Toolbar className={classes.toolbar}>
+                        <div>
+                            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                                <MenuIcon />
+                            </IconButton>
+                            <Button color="inherit" component={Link} to="/posts">
+                                <Typography variant="h6" color="inherit" className={classes.grow}>
+                                    Posts
                             </Typography>
-                        </Button>
+                            </Button>
+                        </div>
                         {this.renderAuth()}
                     </Toolbar>
                 </AppBar>
@@ -81,5 +159,5 @@ function mapStateToProps(state) {
 
 export default compose(
     withStyles(styles, { name: 'AppBar' }),
-    connect(mapStateToProps, { getCurrentUserData })
+    connect(mapStateToProps, { logoutUser })
 )(AppBarTop);
